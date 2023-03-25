83efa42463af04cbc7d485a13e22a8744f9f5111
Nandyaurnob <aurnobnandy@gmail.com>
2023-03-25 23:36:09 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($){
+
+
+      $(".case-studies-carousel").owlCarousel({
+
+          items:4,
+          loop:true,
+          pagination:true,
+          nav:false,
+
+        });
+
+
+
+      });
+
+
+      jQuery(window).load(function(){
+   
+   
+      });
+   
+   
+}(jQuery));
